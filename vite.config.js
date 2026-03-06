6b9d6b11f5108407e84d863a07c64de3a452e002
cai4569 <169347909@qq.com>
2026-03-06 16:06:16 +0800

    初始化

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/vue-Project-2026/', // 👈 重点：必须是 /仓库名/，前后都有斜杠！
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
+  base: '/vue-Project-2026/',
 })
